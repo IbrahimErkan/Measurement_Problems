6d50a4ccd4b5bc207c64d0c133b6eb81a631064d
ibrahimerkan <erkanibrahim99@gmail.com>
2023-03-16 23:36:25 +0300

    mp

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -5,7 +5,8 @@
 ##############################################
 # Uygulama: Kurs Sıralama
 ##############################################
-
+import math
+import scipy.stats as st
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler  # Standartlaştırma
 
@@ -74,3 +75,43 @@ df.sort_values("weighted_sorting_score", ascending=False).head()
 
 df[df["course_name"].str.contains("Veri Bilimi")].\
     sort_values("weighted_sorting_score", ascending=False).head(20)   # kurs isimlerinde "Veri Bilimi" geçenleri getir
+
+
+#####################
+# Bayesian Average Rating Score
+#####################
+# not: Bu yöntem her bir kurs için ayrı ayrı olan puanların dağılım bilgisini kullanarak bize bir average rating ortalama puan hesabı yapacak.
+
+
+# sorting Product with 5 Star Rated          # 5 Yıldız Derecelendirmeli Ürün sıralama
+# Sorting Product According to Distribution of 5 Star Rating        # Ürünü 5 Yıldız Puanı Dağılımı'na Göre Sıralama
+
+def bayesian_average_rating(n, confidence=0.95):
+    if sum(n) == 0:
+        return 0
+    K = len(n)
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    N = sum(n)
+    first_part = 0.0
+    second_part = 0.0
+    for k, n_k in enumerate(n):
+        first_part += (k + 1) * (n[k] + 1) / (N + K)
+        second_part += (k + 1) * (k + 1) * (n[k] + 1) / (N + K)
+    score = first_part - z * math.sqrt((second_part - first_part * first_part) / (N + K + 1))
+    return score
+
+df.head()
+
+df["bar_score"] = df.apply(lambda x: bayesian_average_rating(x[["1_point",
+                                                                "2_point",
+                                                                "3_point",
+                                                                "4_point",
+                                                                "5_point"]]), axis=1)
+
+df.sort_values("weighted_sorting_score", ascending=False).head(20)
+df.sort_values("bar_score", ascending=False).head(20)
+
+df[df["course_name"].index.isin([5, 1])]      # kurs isimlenrinden indexlere göre 5. ve 1. indexdeki kurslara getir.
+
+df[df["course_name"].index.isin([5, 1])].sort_values("bar_score", ascending=False)
+
